791c3e4d51e603af8c7fb6d9a3c6e379a6c59c30
Marcus Lee <mokcandy97@gmail.com>
2022-05-22 02:58:18 -0600

    ADD Overflow

--- Overflow/obj/Debug/net6.0/Overflow.AssemblyInfo.cs
+++ Overflow/obj/Debug/net6.0/Overflow.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Overflow examples")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Overflow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Overflow examples")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Overflow examples")]
+[assembly: System.Reflection.AssemblyProductAttribute("Overflow")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Overflow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
