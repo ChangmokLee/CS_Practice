5a21c4b4c0889233f67ccc43aea5bf49ba9f5c53
Marcus Lee <mokcandy97@gmail.com>
2022-05-22 02:54:52 -0600

    Add overflow

---
+++ Overflow/Overflow examples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Overflow
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            uint a = uint.MaxValue; // Max value of uint -> 4294967295
+
+            Console.WriteLine(a);
+
+            a = a + 1;
+
+            Console.WriteLine(a);
+        }
+    }
+}
