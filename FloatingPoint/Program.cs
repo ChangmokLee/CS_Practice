2d873a0b4f541edf40252edecfa674aed576581d
Marcus Lee <mokcandy97@gmail.com>
2022-05-22 03:19:55 -0600

    ADD decimal

--- FloatingPoint/Program.cs
+++ FloatingPoint/Program.cs
@@ -7,10 +7,12 @@ namespace FloatingPoint
         static void Main(string[] args)
         {
             float a = 3.1415_9265_3589_7932_3846f;
-            Console.WriteLine(a);
-
             double b = 3.1415_9265_3589_7932_3846;
-            Console.Write(b);
+            decimal c = 3.1415_9265_3589_7932_3846m;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
         }
     }
 }
