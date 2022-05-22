04f4bff71c7393bc58d5c918044aae6249a4635e
Marcus Lee <mokcandy97@gmail.com>
2022-05-22 03:28:05 -0600

    ADD string

---
+++ char/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Char
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            char a = 'H';
+            char b = 'e';
+            char c = 'l';
+            char d = 'l';
+            char e = 'o';
+
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.Write(e);
+            Console.WriteLine();
+
+            string f = "Nice to meet you!";
+            string g = "My name is Marcus.";
+
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+        }
+    }
+}
